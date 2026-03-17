14ca0dc4c9075a6f17a1fe7570a7d7c8f7206a52
lucasreydman <lucasreydman@gmail.com>
2026-03-17 12:03:04 -0300

    vercel

--- frontend/frontend/.next/dev/static/chunks/frontend_app_page_tsx_01cf8f8c._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/9e883_cff4302c._.js",
-  "static/chunks/frontend_f2c0fdc5._.js"
-],
-    source: "dynamic"
-});
